a363e858e5e78bc7ff2824419d86c0983ab34145
Ane Paixão <anepaixao50@gmail.com>
2025-10-10 02:35:03 -0300

    Integrando codigos

--- Trabalho final/cliente.h
+++ Trabalho final/cliente.h
@@ -5,4 +5,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Prototipos das funcoes implementadas em cliente.c
+int login(void);
+int tela_principal(void);
+double deposito(double n, int interacoes);
+void pix_aleatorio(void);
+int pix_cadastro(void);
+int pix_cadastrados(void);
+void qr_code(void);
+void boleto(void);
+int extrato(void);
+double saque(double valor, int max_tentativas);
+int opcoes(char *escolha);
+
 #endif
--- Trabalho final/main.c
+++ Trabalho final/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "banco.h" 
-#include "cliente.c"
+#include <string.h>
+#include "banco.h"
+#include "cliente.h"
 
 int main(void) {
     int opcao;
@@ -9,16 +10,31 @@ int main(void) {
     do {
         printf("\n===== SISTEMA BANCARIO =====\n");
         printf("1. Acessar modulo do Administrador\n");
+        printf("2. Acessar modulo do Cliente\n");
         printf("0. Sair\n");
         printf("Escolha uma opcao: ");
-        scanf("%d", &opcao);
+        if (scanf("%d", &opcao) != 1) {
+            int c; while ((c = getchar()) != '\n' && c != EOF) {}
+            opcao = -1;
+        }
 
         switch (opcao) {
             case 1:
-                if (autenticarAdministrador()) {
-                    menuAdministrador();
+                if (autenticarAdministrador()) menuAdministrador();
+                break;
+            case 2: {
+                char escolha[64] = "";
+                // limpar newline pendente
+                int ch; while ((ch = getchar()) != '\n' && ch != EOF) {}
+                login();
+                tela_principal();
+                printf("DIGITE A OPCAO QUE DESEJA: ");
+                if (fgets(escolha, sizeof(escolha), stdin) != NULL) {
+                    escolha[strcspn(escolha, "\n")] = '\0';
+                    opcoes(escolha);
                 }
                 break;
+            }
             case 0:
                 printf("Encerrando o sistema...\n");
                 break;
@@ -30,20 +46,3 @@ int main(void) {
     return 0;
 }
 
-//Parte do usuario///
-
-int main(void) {
-    char escolhar[20];  
-   
-    login();
-    tela_principal();
-    
-    
-    puts("DIGITE A OPÇÃO QUE DESEJA: "); 
-    scanf(" %[^\n]", escolhar);
-  
-    opcoes(escolhar);
-  
-    printf("Obrigada por usar o banco!");
-    return 0;
-}
